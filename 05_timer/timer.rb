bf105826e9aa677f1478a2660f33f2cad5df8918
MattMonta <matthieumontagne@gmail.com>
2018-10-08 17:41:19 +0200

    Fini

--- 05_timer/timer.rb
+++ 05_timer/timer.rb
@@ -1,16 +1,3 @@
-class Timer
-  def seconds=(sec)
-    @seconds = sec
-  end
-
-  def seconds
-    @seconds || 0
-  end
-
-  def time_string
-    h = @seconds / 3600
-    m = (@seconds - h*3600) / 60
-    s = @seconds - h*3600 - m*60
-    "%02d:%02d:%02d" % [h,m,s]
-  end
+def time_string(total_seconds)
+ Time.at(total_seconds).utc.strftime("%H:%M:%S")
 end
